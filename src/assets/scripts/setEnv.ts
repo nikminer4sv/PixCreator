988bada337d186b1ac70a0660201f5be8698703f
Nikita <nikminer4sv@gmail.com>
2023-04-05 18:05:39 +0200

    refactoring

--- src/assets/scripts/setEnv.ts
+++ src/assets/scripts/setEnv.ts
@@ -8,8 +8,6 @@ const { argv } = require('yargs');
 require('dotenv').config();
 const environment = argv.environment;
 
-console.log(process.env)
-
 function writeFileUsingFS(targetPath, environmentFileContent) {
   writeFile(targetPath, environmentFileContent, function (err) {
     if (err) {
